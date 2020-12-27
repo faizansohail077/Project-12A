c09d78158893b47cbe0b21e814629197766b32b0
Muhammad Faizan <faizansohail077@gmail.com>
2020-12-27 03:50:30 -0500

    Project 12A

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+    <Link to="/blog">go to blog</Link>
+  </Layout>
+)
+
+export default IndexPage
